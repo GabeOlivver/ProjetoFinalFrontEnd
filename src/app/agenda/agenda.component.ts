4c2bad45de327de3ca4616d4e5f412843b26a5b4
GabeOlivver <gabriel.oliveira294@fatec.sp.gov.br>
2024-07-01 20:08:49 -0300

    commit 7 - json server e serviços

--- src/app/agenda/agenda.component.ts
+++ src/app/agenda/agenda.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Contato } from '../contato';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ContatoService } from '../contato.service';
 
 @Component({
   selector: 'app-agenda',
   templateUrl: './agenda.component.html',
   styleUrl: './agenda.component.css'
 })
-export class AgendaComponent {
+export class AgendaComponent implements OnInit {
   contatos: Contato[]=[];
   formGroupContato: FormGroup;
 
-  constructor(private formBuilder:FormBuilder){
+  isEditing: boolean = false;
+
+  ngOnInit(): void {
+      this.loadContatos();
+  }
+
+  loadContatos(){
+    this.service.getContatos().subscribe({
+      next: data => this.contatos = data
+    })
+  }
+
+  constructor(private formBuilder:FormBuilder, private service: ContatoService){
     this.formGroupContato = formBuilder.group({
       id: [''],
       nome: [''],
@@ -25,6 +38,34 @@ export class AgendaComponent {
   }
 
   save(){
-    this.contatos.push(this.formGroupContato.value);
+    if(this.isEditing){
+      this.service.save(this.formGroupContato.value).subscribe({
+        next: () => {
+          this.loadContatos();
+          this.isEditing = false;
+          this.formGroupContato.reset();
+        }
+      })
+    } else{
+      this.service.save(this.formGroupContato.value).subscribe({
+        next: (data) => {
+          this.contatos.push(data);
+          this.formGroupContato.reset();
+        }
+      })
+    }
   }
+
+  delete(contato:Contato){
+    this.service.delete(contato).subscribe({
+      next: () => this.loadContatos()
+    })
+  }
+
+  edit(contato: Contato){
+    this.formGroupContato.setValue(contato);
+    this.isEditing = true;
+  }
+
+
 }
