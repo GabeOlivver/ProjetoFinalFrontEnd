49f3d62d97468931545cb22139a0e7e316df911e
GabeOlivver <gabriel.oliveira294@fatec.sp.gov.br>
2024-07-01 08:59:26 -0300

    commit 3 - componentes principais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AgendaComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
